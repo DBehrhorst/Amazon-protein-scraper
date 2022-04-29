76d796e2ef0d00421de2e44e85a8781a39d7504a
Foang <101474843+Foanng@users.noreply.github.com>
2022-04-29 17:02:46 -0400

    updating the if__name__

--- cvsproteinscraper.py
+++ cvsproteinscraper.py
@@ -18,7 +18,7 @@ class ScrapeInfo:
         -prints information into a dictionary
 
     """
-    
+
     def webscrape(self):
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'}
         url = 'https://www.cvs.com'
@@ -56,5 +56,5 @@ class ScrapeInfo:
         
 #defining the if__name__ == "__main__ function"
 if __name__ == "__main__":        
-    x = scrape_proteins()
+    x = ScrapeInfo()
     x.webscrape()
